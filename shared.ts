6a0c5d658afc1837931c05c44cee34b7bd4ec370
Daniel Whitford <whitford.dsw@gmail.com>
2020-10-29 23:10:31 +0000

    Auto deploy

--- shared.ts
+++ shared.ts
@@ -1,11 +1,15 @@
-
-const competitions = ["Premier League", "Champions League", "Europa League", "Championship"]
+const competitions = [
+  "Premier League",
+  "Champions League",
+  "Europa League",
+  "Championship",
+];
 
 export function competitionNormaliser(name: string) {
-    for(let comp of competitions) {
-        if (name.startsWith(comp)) {
-            return comp
-        }
+  for (let comp of competitions) {
+    if (name.startsWith(comp)) {
+      return comp;
     }
-    throw new Error("Competition not recognised")
+  }
+  throw new Error("Competition not recognised");
 }
