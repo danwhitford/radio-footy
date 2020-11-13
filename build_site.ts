786891813e4180aba8012fb806eec142034c3e1a
danielwhitford <whitford.dsw@gmail.com>
2020-11-13 00:42:15 +0000

    look ma no files

--- build_site.ts
+++ build_site.ts
@@ -1,33 +1,16 @@
 import * as pug from "pug";
 import * as fs from "fs";
 import { utcToZonedTime } from "date-fns-tz";
-import { from, of, zip } from "rxjs";
+import { concat, from, of, zip } from "rxjs";
 import { groupBy, map, mergeAll, mergeMap, tap, toArray } from "rxjs/operators";
 import { fromEntries } from "./shared";
+import bbcObservable from "./get_bbc";
+import tsObservable from "./get_ts";
 
-const files = ["data/ts.json", "data/5live.json"];
-let matches = [];
+// const files = ["data/ts.json", "data/5live.json"];
 
-for (let file of files) {
-  const fd = fs.openSync(file, "r");
-  const content = fs.readFileSync(fd);
-  fs.closeSync(fd);
-  const m = JSON.parse(content.toString("binary"));
-  matches = matches.concat(m);
-}
-
-matches.sort((m, mm) => {
-  return new Date(m.datetime).valueOf() - new Date(mm.datetime).valueOf();
-});
-
-const matchObservable = from(files)
+const matchObservable = concat(bbcObservable, tsObservable)
   .pipe(
-    map((f) => {
-      const fd = fs.openSync(f, "r");
-      const content = fs.readFileSync(fd);
-      fs.closeSync(fd);
-      return JSON.parse(content.toString("binary"));
-    }),
     mergeAll(),
     toArray(),
     map((ar: any[]) =>
