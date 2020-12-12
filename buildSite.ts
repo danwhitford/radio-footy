d7dfecea365368bbc505b4512a60f417c646f07e
Daniel Whitford <whitford.dsw@gmail.com>
2020-12-12 13:03:39 +0000

    Auto deploy

--- buildSite.ts
+++ buildSite.ts
@@ -7,36 +7,55 @@ function urlEncode(s: string) {
 }
 
 function build(buildFn, data, out) {
-  const page = buildFn(data)
+  const page = buildFn(data);
   const fd = fs.openSync(out, "w");
   fs.writeFileSync(fd, page);
   fs.closeSync(fd);
 }
 
 function buildListingsPage(template: string, data: any, out: string) {
-  const fn = pug.compileFile(template)
-  build(fn, {
-    matches: data,
-    competitions,
-    stations,
-    teams,
-    urlEncode,
-  }, out)
-}
-
-function buildDirIndexPage(template: string, directory: string, data: any, out: string) {
   const fn = pug.compileFile(template);
-  build(fn, {
-    directory: directory,
-    pages: data,
-    competitions,
-    stations,
-    teams,
-    urlEncode,
-  }, out);
+  build(
+    fn,
+    {
+      matches: data,
+      competitions,
+      stations,
+      teams,
+      urlEncode,
+    },
+    out
+  );
 }
 
-function buildFilteredListingsPage(template: string, data: any, outDir: string, els: any[], filterFunc: any) {
+function buildDirIndexPage(
+  template: string,
+  directory: string,
+  data: any,
+  out: string
+) {
+  const fn = pug.compileFile(template);
+  build(
+    fn,
+    {
+      directory: directory,
+      pages: data,
+      competitions,
+      stations,
+      teams,
+      urlEncode,
+    },
+    out
+  );
+}
+
+function buildFilteredListingsPage(
+  template: string,
+  data: any,
+  outDir: string,
+  els: any[],
+  filterFunc: any
+) {
   for (const el of els) {
     const filtered = data
       .map((day) => {
@@ -47,7 +66,7 @@ function buildFilteredListingsPage(template: string, data: any, outDir: string, 
       })
       .filter((day) => day.matches.length > 0);
     const addy = urlEncode(el);
-    buildListingsPage(template, filtered, `${outDir}/${addy}.html`)
+    buildListingsPage(template, filtered, `${outDir}/${addy}.html`);
   }
 }
 
@@ -68,11 +87,44 @@ let stations = JSON.parse(data)
   .filter((x, i, a) => a.indexOf(x) == i)
   .sort();
 
-buildListingsPage("templates/listings.pug", siteData, 'site/index.html')
-buildFilteredListingsPage("templates/listings.pug", siteData, 'site/teams', teams, (el) => (match) => match.title.split(" v ").includes(el))
-buildFilteredListingsPage("templates/listings.pug", siteData, 'site/stations', teams, (el) => (match) => match.station === el)
-buildFilteredListingsPage("templates/listings.pug", siteData, 'site/competitions', teams, (el) => (match) => match.competition === el)
+buildListingsPage("templates/listings.pug", siteData, "site/index.html");
+buildFilteredListingsPage(
+  "templates/listings.pug",
+  siteData,
+  "site/teams",
+  teams,
+  (el) => (match) => match.title.split(" v ").includes(el)
+);
+buildFilteredListingsPage(
+  "templates/listings.pug",
+  siteData,
+  "site/stations",
+  teams,
+  (el) => (match) => match.station === el
+);
+buildFilteredListingsPage(
+  "templates/listings.pug",
+  siteData,
+  "site/competitions",
+  teams,
+  (el) => (match) => match.competition === el
+);
 
-buildDirIndexPage('templates/dir-index.pug', 'stations', stations, 'site/stations/index.html')
-buildDirIndexPage('templates/dir-index.pug', 'teams', teams, 'site/teams/index.html')
-buildDirIndexPage('templates/dir-index.pug', 'competitions', competitions, 'site/competitions/index.html')
+buildDirIndexPage(
+  "templates/dir-index.pug",
+  "stations",
+  stations,
+  "site/stations/index.html"
+);
+buildDirIndexPage(
+  "templates/dir-index.pug",
+  "teams",
+  teams,
+  "site/teams/index.html"
+);
+buildDirIndexPage(
+  "templates/dir-index.pug",
+  "competitions",
+  competitions,
+  "site/competitions/index.html"
+);
