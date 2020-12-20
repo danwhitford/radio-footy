64da12562f2fdd0ba8f0c88d30b02c8401d29e3b
Daniel Whitford <whitford.dsw@gmail.com>
2020-12-20 11:01:35 +0000

    Auto deploy

--- observables/shared.ts
+++ observables/shared.ts
@@ -6,7 +6,7 @@ const competitions = [
   "FA Cup",
   "English Football League Trophy",
   "International Football",
-    "EFL Cup",
+  "EFL Cup",
 ];
 
 export function normaliseCompetitionName(name: string) {
