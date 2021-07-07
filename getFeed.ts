c4fb11af2eb326cf3eaf221d3b3419d51989e947
Dan Whitford <dan.whitford@fanduel.com>
2021-07-07 09:51:25 +0100

    Add ts config for build

--- getFeed.ts
+++
@@ -1,9 +0,0 @@
-import * as pug from "pug";
-import * as fs from "fs";
-import matchesObservable from "./observables/matchesObservable";
-
-matchesObservable.subscribe((v) => {
-  const fd = fs.openSync("./docs/data.json", "w");
-  fs.writeFileSync(fd, JSON.stringify(v));
-  fs.closeSync(fd);
-});
