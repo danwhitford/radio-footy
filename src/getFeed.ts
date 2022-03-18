deb7aaacb7a972ca0d59e3c24103d2cc1a5e642d
Dan Whitford <dan.whitford@fanduel.com>
2022-03-18 11:03:53 +0000

    Talksport feed in go

--- src/getFeed.ts
+++
@@ -1,8 +0,0 @@
-import * as fs from "fs";
-import matchesObservable from "./observables/matchesObservable";
-
-matchesObservable.subscribe((v) => {
-  const fd = fs.openSync("./docs/data.json", "w");
-  fs.writeFileSync(fd, JSON.stringify(v));
-  fs.closeSync(fd);
-});
