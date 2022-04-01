e4de07612611827d85080a8eb90b975285ba91f2
danwhitford <18026053+danwhitford@users.noreply.github.com>
2022-04-01 13:06:51 +0000

    Deploying to gh-pages from  @ d65ef7d834917d094d27834f5dabf6d130c2234b 🚀

---
+++ serviceWorker.js
@@ -0,0 +1,21 @@
+const addResourcesToCache = async (resources) => {
+    const cache = await caches.open('v1');
+    await cache.addAll(resources);
+};
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        addResourcesToCache([
+            '/',
+            '/index.html',
+        ])
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request);
+      })
+    );
+  });
