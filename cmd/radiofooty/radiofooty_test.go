bfd2c86f849e130250b28f41967b9857474de3fb
Daniel Whitford <whitford.dsw@gmail.com>
2023-08-23 21:23:17 +0100

    no more classes anymore

--- cmd/radiofooty/radiofooty_test.go
+++ cmd/radiofooty/radiofooty_test.go
@@ -2,112 +2,11 @@ package main
 
 import (
 	"bytes"
-	"regexp"
 	"testing"
-	"time"
 
 	"github.com/google/go-cmp/cmp"
 	"whitford.io/radiofooty/internal/feeds"
 )
-
-func TestWriteIndex(t *testing.T) {
-	table := []struct {
-		input struct {
-			MatchDays []feeds.MergedMatchDay
-		}
-		output string
-	}{
-		{
-			input: struct {
-				MatchDays []feeds.MergedMatchDay
-			}{
-				MatchDays: []feeds.MergedMatchDay{
-					{
-						NiceDate: "Monday, May 15",
-						DateTime: time.Date(2023, 5, 15, 0, 0, 0, 0, time.UTC),
-						Matches: []feeds.MergedMatch{
-							{
-								Title:       "Southampton v Manchester City",
-								Datetime:    "2023-05-15T15:00:00Z",
-								Competition: "Premier League",
-								Stations:    []string{"talkSPORT", "BBC Radio 5 Live"},
-								Time:        "15:00",
-								Date:        "Monday, May 15",
-							},
-						},
-					},
-					{
-						NiceDate: "Tuesday, May 16",
-						DateTime: time.Date(2023, 5, 16, 0, 0, 0, 0, time.UTC),
-						Matches: []feeds.MergedMatch{
-							{
-								Title:       "England v Australia",
-								Datetime:    "2023-05-15T10:00:00Z",
-								Competition: "The Ashes",
-								Stations:    []string{"BBC Radio 5 Live", "BBC Radio 5 Extra"},
-								Time:        "10:00",
-								Date:        "Tuesday, May 16",
-							},
-						},
-					},
-				},
-			},
-			output: `<!DOCTYPE html>
-<html lang="en-gb">
-
-<head>
-    <title>Wireless Football</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="Description" content="Upcoming football matches on the wireless">
-    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
-    <meta content="utf-8" http-equiv="encoding">
-</head>
-
-<body>
-    <div id="container">
-        <h1>Football on radio and TV</h1>
-        
-        <div class="matchday">
-            <h2>Monday, May 15</h2>
-            
-                <div class="match">
-                    <p class="row text-row"><b>15:00 | talkSPORT | BBC Radio 5 Live</b></p>
-                    <p class="row text-row">Southampton v Manchester City (Premier League)</p>
-                </div>
-            
-        </div>
-        <hr />
-        
-        <div class="matchday">
-            <h2>Tuesday, May 16</h2>
-            
-                <div class="match">
-									<p class="row text-row"><b>10:00 | BBC Radio 5 Live | BBC Radio 5 Extra</b></p>
-									<p class="row text-row">England v Australia (The Ashes)</p>
-								</div>
-            
-        </div>
-        <hr />
-        
-    </div>
-</body>
-</html>
-`,
-		},
-	}
-
-	for _, tst := range table {
-		var buffer bytes.Buffer
-		writeIndex(tst.input, "../../internal/website/template.go.tmpl", &buffer)
-		re := regexp.MustCompile(`\s+`)
-		want := re.ReplaceAllString(tst.output, " ")
-		got := re.ReplaceAllString(buffer.String(), " ")
-
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Errorf("writeIndex(%v) mismatch (-want +got):\n%s", tst.input, diff)
-		}
-	}
-}
 
 func TestWriteCal(t *testing.T) {
 	table := []struct {
