65d668075986b5c232488906bd0b536530b2ac35
Dan Whitford <whitford.dsw@gmail.com>
2023-09-03 14:05:51 +0100

    better logging for teapots

--- internal/filecacher/filecacher.go
+++ internal/filecacher/filecacher.go
@@ -28,7 +28,11 @@ func getAndSave(url, fname string, getter Getter) ([]byte, error) {
 		return nil, err
 	}
 	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("error getting url: %s. code: %d. body: %v", url, resp.StatusCode, resp.Body)
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("error reading body from error state: %w", err)
+		}
+		return nil, fmt.Errorf("error getting url: %s. code: %d. body: %s", url, resp.StatusCode, string(body))
 	}
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
